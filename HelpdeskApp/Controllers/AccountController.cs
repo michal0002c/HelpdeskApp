44c83414c07cedc9c9e50b24e8a55466cc360c30
michal0002c <michal.kasperek0002c@gmail.com>
2025-03-20 21:47:21 +0100

    Simple login functionality, model and controller

---
+++ HelpdeskApp/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using HelpdeskApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelpdeskApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(UserModel user)
+        {
+            if (user.UserName == "admin" && user.Password == "admin") 
+            {
+            return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Error = "Incorrect username or password.";
+                return View();
+            }
+        }
+    }
+}
